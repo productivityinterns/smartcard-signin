76edc7a51c88fca446712bdb4c99601534458873
Connorapple <connorapple@gmail.com>
2019-07-09 10:58:32 -0400

    Reworked the scanning loop to work with the barcode reader instead of the NFC

--- scanner.py
+++ scanner.py
@@ -1,8 +1,30 @@
 import nfc
-from nfc.clf import RemoteTarget
+#from nfc.clf import RemoteTarget
+from cacbarcode import PDF417Barcode, Code39Barcode
 
-clf = nfc.ContactlessFrontend('usb')
-userInput = ""
+def getEDIPI(data):
+    try:
+        barcode = PDF417Barcode(data)
+        edipi = barcode.edipi
+    except:
+        # Try the other barcode type
+        try:
+            barcode = Code39Barcode(data)
+            edipi = barcode.edipi
+        except:
+            # Neither barcode was correct
+            # Blink failure light
+            # make failure noise
+            print("Neither barcode worked!")
+    return edipi
+#clf = nfc.ContactlessFrontend('usb')
+barcode_data = ""
+edipi = None
 while True:
-	tag = clf.connect(rdwr={'on-connect': lambda tag: False})
-	print(tag)
+    barcode_data = input("scan it")
+    edipi = getEDIPI(barcode_data)
+    # Signin API call here, pass in edipi
+    print("EDIPI =", edipi)
+
+    	#tag = clf.connect(rdwr={'on-connect': lambda tag: False})
+    	#print(tag)
