94d3931cd2af8fbc60fd14641da2405dc38d9334
Matt G <matthew.galligan@gmail.com>
2019-07-10 23:38:55 -0400

    Added a realish method that might work, but needs testing.

--- attendance/attendance.py
+++ attendance/attendance.py
@@ -1,5 +1,5 @@
 from enum import Enum
-import random, time
+import random, time, pymssql, os
 ## used to check in or check out a user
 
 class StatusValues(Enum):
@@ -16,7 +16,24 @@ class Attendance:
         pass
     
     def checkin_checkout(self,identifier):
-        pass
+        """
+        Checkin / Checkout from the back end system using the SIGNIN_ username/password/server/database details in environment
+        this executes a spro called spCheckinCheckout with a unique identifier, and then returns a returnValue that matches the values in StatusValues
+        """
+        username = os.environ['SIGNIN_USERNAME']
+        password = os.environ['SIGNIN_PASSWORD']
+        server = os.environ['SIGNIN_SERVER']
+        database = os.environ['SIGNIN_DATABASE']
+
+        if username is None or password is None or server is None or database is None:
+            print("Could not find necessary environment variables for backend connection.")
+            return StatusValues.Error
+
+        with pymssql.connect(server=server, user=username, password=password, database=database) as conn:
+            with conn.cursor() as cursor:                   
+                cursor.callproc('dbo.spCheckinCheckout', (identifier,))
+                for row in cursor:
+                    return row['returnValue']
 
 
 class MockAttendance:
